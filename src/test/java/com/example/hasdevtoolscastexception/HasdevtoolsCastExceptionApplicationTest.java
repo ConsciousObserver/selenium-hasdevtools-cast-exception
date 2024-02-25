68d8b2d19d04785c6cd3351343aba3a53e515b33
noname <nomail@nomail.com>
2024-02-25 20:39:50 +0530

    working

--- src/test/java/com/example/hasdevtoolscastexception/HasdevtoolsCastExceptionApplicationTest.java
+++ src/test/java/com/example/hasdevtoolscastexception/HasdevtoolsCastExceptionApplicationTest.java
@@ -2,6 +2,8 @@ package com.example.hasdevtoolscastexception;
 
 import java.util.Optional;
 
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
@@ -10,63 +12,67 @@ import org.openqa.selenium.devtools.HasDevTools;
 import org.openqa.selenium.devtools.v118.network.Network;
 import org.openqa.selenium.remote.Augmenter;
 import org.openqa.selenium.remote.RemoteWebDriver;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.testcontainers.containers.BrowserWebDriverContainer;
 import org.testcontainers.containers.SocatContainer;
 
 //@Testcontainers
 public class HasdevtoolsCastExceptionApplicationTest {
-    org.testcontainers.containers.Network socatNetwork = org.testcontainers.containers.Network.newNetwork();
+    static Logger log = LoggerFactory.getLogger(HasdevtoolsCastExceptionApplicationTest.class);
 
-    //    @Container
-    //    SocatContainer socatContainer = new SocatContainer()
-    //            .withTarget(2000, "chrome", 4444)
-    //            .withNetwork(socatNetwork);
+    static org.testcontainers.containers.Network socatNetwork = org.testcontainers.containers.Network.newNetwork();
 
-    //    {
-    //        network = Network.newNetwork();
-    //
-    //        socatContainer = new SocatContainer()
-    //                .withTarget(2000, "chrome", 4444)
-    //                .withNetwork(network);
-    //        socatContainer.start();
-    //    }
+    static SocatContainer tcpProxySocatContainer;
 
-    //    @Container
-    //    BrowserWebDriverContainer<?> browserWebDriverContainer = new BrowserWebDriverContainer<>()
-    //            .dependsOn(socatContainer)
-    //            .withCapabilities(new ChromeOptions())
-    //            .withEnv("SE_NODE_GRID_URL", String.format("%s:%d", socatContainer.getHost(), socatContainer.getMappedPort(2000)))
-    //            .withNetworkAliases("chrome")
-    //            .withNetwork(socatNetwork);
-    //            .withRecordingMode(VncRecordingMode.RECORD_ALL, new File("target"))
-    //            .withRecordingFileFactory(new DefaultRecordingFileFactory());
+    static BrowserWebDriverContainer<?> browserWebDriverContainer;
+
+    @BeforeAll
+    static void setUp() {
+        log.info("**************** Test setup start");
+
+        log.info("**************** Starting Socat TCP proxy container");
+
+        tcpProxySocatContainer = new SocatContainer()
+                .withTarget(2000, "chrome", 4444)
+                .withNetwork(socatNetwork);
+
+        tcpProxySocatContainer.start();
+
+        log.info("**************** Starting Selenium  container");
+
+        browserWebDriverContainer = new BrowserWebDriverContainer<>()
+                .dependsOn(tcpProxySocatContainer)
+                .withCapabilities(new ChromeOptions())
+                .withEnv("SE_NODE_GRID_URL",
+                        String.format("http://%s:%d", tcpProxySocatContainer.getHost(),
+                                tcpProxySocatContainer.getMappedPort(2000)))
+                .withNetworkAliases("chrome")
+                .withNetwork(socatNetwork);
+
+        browserWebDriverContainer.start();
+
+        log.info("**************** Test setup end");
+    }
+
+    @AfterAll
+    static void tearDown() {
+        log.info("**************** Stopping containers");
+        
+        if (tcpProxySocatContainer != null) {
+            tcpProxySocatContainer.stop();
+        }
+
+        if (browserWebDriverContainer != null) {
+            browserWebDriverContainer.stop();
+        }
+    }
 
     @Test
     void test() {
-        SocatContainer socatContainer = null;
-        BrowserWebDriverContainer<?> browserWebDriverContainer = null;
         RemoteWebDriver webDriver = null;
 
         try {
-            socatContainer = new SocatContainer()
-                    .withTarget(2000, "chrome", 4444)
-                    .withNetwork(socatNetwork);
-            //                    .withStartupCheckStrategy(
-            //                            new OneShotStartupCheckStrategy().withTimeout(Duration.ofSeconds(60))
-            //                        );
-
-            socatContainer.start();
-
-            browserWebDriverContainer = new BrowserWebDriverContainer<>()
-                    .dependsOn(socatContainer)
-                    .withCapabilities(new ChromeOptions())
-                    .withEnv("SE_NODE_GRID_URL",
-                            String.format("http://%s:%d", socatContainer.getHost(), socatContainer.getMappedPort(2000)))
-                    .withNetworkAliases("chrome")
-                    .withNetwork(socatNetwork);
-
-            browserWebDriverContainer.start();
-
             ChromeOptions capabilities = new ChromeOptions().addArguments("--headless");
             webDriver = new RemoteWebDriver(browserWebDriverContainer.getSeleniumAddress(), capabilities);
             //            webDriver.getCapabilities().getCapability("se:cdp");
@@ -75,12 +81,11 @@ public class HasdevtoolsCastExceptionApplicationTest {
             devTools.createSession();
 
             devTools.send(Network.enable(Optional.of(1000000), Optional.empty(), Optional.empty()));
-            
+
             devTools.addListener(Network.responseReceived(), responseReceived -> {
                 System.out.println("status: " + responseReceived.getResponse().getStatus() + ", Url: "
                         + responseReceived.getResponse().getUrl());
             });
-            //            System.out.println("Devtools: " + devTools);
 
             System.out.println("VNC address : " + browserWebDriverContainer.getVncAddress());
             webDriver.get("https://stackoverflow.com");
@@ -93,40 +98,6 @@ public class HasdevtoolsCastExceptionApplicationTest {
             if (webDriver != null) {
                 webDriver.quit();
             }
-
-            if (socatContainer != null) {
-                socatContainer.stop();
-            }
-
-            if (browserWebDriverContainer != null) {
-                browserWebDriverContainer.stop();
-            }
         }
-
-        //        System.out.println("Running Test");
-        //        RemoteWebDriver webDriver = null;
-        //        try {
-        //            ChromeOptions capabilities = new ChromeOptions().addArguments("--headless");
-        //            webDriver = new RemoteWebDriver(browserWebDriverContainer.getSeleniumAddress(), capabilities);
-        ////            webDriver.getCapabilities().getCapability("se:cdp");
-        //            WebDriver augmentedWebDriver = new Augmenter().augment(webDriver);
-        //            DevTools devTools = ((HasDevTools) augmentedWebDriver).getDevTools();
-        //            devTools.createSession();
-        ////            System.out.println("Devtools: " + devTools);
-        //            
-        //            System.out.println("VNC address : " + browserWebDriverContainer.getVncAddress());
-        //            webDriver.get("https://stackoverflow.com");
-        //
-        //            System.out.println(webDriver.getTitle());
-        //            
-        ////            webDriver.getClass().getInterfaces();
-        //        } catch (Exception ex) {
-        //            ex.printStackTrace();
-        //            throw ex;
-        //        } finally {
-        //            if (webDriver != null) {
-        //                webDriver.quit();
-        //            }
-        //        }
     }
 }
